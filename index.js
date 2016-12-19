3a8e0630eb7da04083f6485a3a5515659e79d671
Brandon Schmuck <bschmuck@Brandons-MacBook-4.local>
2016-12-19 15:41:22 -0500

    Updated structure to use Angular.JS

--- index.js
+++ index.js
@@ -1,84 +1,108 @@
-var pages = ["#home", "#brothersPage", "#alumniPage", "#aboutPage", "#contactPage"];
-var images = ["ato1.jpg", "ato2.jpg", "ato3.jpg", "ato1.jpg", "ato2.jpg", "ato3.jpg"];
+var myApp = angular.module('myApp', ['ngRoute']);
 
-var brothers = [
-	{
-		"name" : "John Doe",
-		"title" : "President",
-		"image" : "images/ato3.jpg"
-	},
-	{
-		"name" : "John Doe",
-		"title" : "President",
-		"image" : "images/ato3.jpg"
-	},
-	{
-		"name" : "John Doe",
-		"title" : "President",
-		"image" : "images/ato3.jpg"
-	},
-	{
-		"name" : "John Doe",
-		"title" : "President",
-		"image" : "images/ato3.jpg"
-	}
-];
-
-$(document).ready(function() {
-	images.forEach(function(image) {
-		$("#photosDiv").append(
-			'<div class="col-xs-6 col-md-4"><a href="#" class="thumbnail"><img src="images/' + image + '" style></a></div>');
-	});	
-
-	brothers.forEach(function(brother) {
-		$("#officersList").append('<div class="col-xs-12 memberDiv" style="background-color:#4A90E2;height:125px;border-radius:100px;box-shadow: inset 0px -1px 5px #000;"><a href="#" class="brotherThumbnail thumbnail col-xs-2"><img class="brotherImage" style="width: 125px;height: 125px;margin-left:-20px;margin-top:-3.5px;" src="' + brother.image + '"></a><div class="col-xs-3"><h3>' + brother.name + '</h3><h4>' + brother.title + '</h3></div></div>');
-	});
-	brothers.forEach(function(brother) {
-		$("#membersList").append('<div class="col-xs-12 memberDiv" style="background-color:#4A90E2;height:125px;border-radius:100px;box-shadow: inset 0px -1px 5px #000;"><a href="#" class="brotherThumbnail thumbnail col-xs-2"><img class="brotherImage" style="width: 125px;height: 125px;margin-left:-20px;margin-top:-3.5px;" src="' + brother.image + '"></a><div class="col-xs-3"><h3>' + brother.name + '</h3><h4>' + brother.title + '</h3></div></div>');
-	});
-	brothers.forEach(function(brother) {
-		$("#nationalAlums").append('<div class="col-xs-12 memberDiv" style="background-color:#4A90E2;height:125px;border-radius:100px;box-shadow: inset 0px -1px 5px #000;"><a href="#" class="brotherThumbnail thumbnail col-xs-2"><img class="brotherImage" style="width: 125px;height: 125px;margin-left:-20px;margin-top:-3.5px;" src="' + brother.image + '"></a><div class="col-xs-3"><h3>' + brother.name + '</h3><h4>' + brother.title + '</h3></div></div>');
-	});
-	brothers.forEach(function(brother) {
-		$("#CMUAlums").append('<div class="col-xs-12 memberDiv" style="background-color:#4A90E2;height:125px;border-radius:100px;box-shadow: inset 0px -1px 5px #000;"><a href="#" class="brotherThumbnail thumbnail col-xs-2"><img class="brotherImage" style="width: 125px;height: 125px;margin-left:-20px;margin-top:-3.5px;" src="' + brother.image + '"></a><div class="col-xs-3"><h3>' + brother.name + '</h3><h4>' + brother.title + '</h3></div></div>');
-	});
+myApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/brothers', {
+        templateUrl:  'pages/brothers.html',
+        controller: 'brothersController'
+    })
+    
+    .when('/alumni', {
+        templateUrl: 'pages/alumni.html',
+        controller: 'alumniController'
+    })
+    
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
+    })
+    
+    .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'contactController'
+    })
 });
 
-var showPage = function(selectedPage) {
-	pages.forEach(function(page) {
-		if(page != selectedPage) {
-			$(page).hide();
-		} else {
-			$(page).show();
-		}
-	});
-}
-$('#eventlist').gCalReader({
-  calendarId:'q8774hmduqesk7mjpt7c5iq530@group.calendar.google.com',
-  apiKey:'AIzaSyCH7F6ChOHVASIiOY6QU4KPCVxjLufu0Jc',
-  dateFormat: 'LongDate',
-  errorMsg: 'No events in calendar',
-  maxEvents: 25,
-  futureEventsOnly: true,
-  sortDescending: false
-});
+myApp.controller('mainController', ['$scope', function($scope) {
+    $scope.photos = [
+        {name: "images/ato1.jpg"},
+        {name: "images/ato2.jpg"},
+        {name: "images/ato3.jpg"}
+    ]
+    
+    $('#eventlist').gCalReader({
+      calendarId:'q8774hmduqesk7mjpt7c5iq530@group.calendar.google.com',
+      apiKey:'AIzaSyCH7F6ChOHVASIiOY6QU4KPCVxjLufu0Jc',
+      dateFormat: 'LongDate',
+      errorMsg: 'No events in calendar',
+      maxEvents: 25,
+      futureEventsOnly: true,
+      sortDescending: false
+    });
+}]);
 
-$("#homeSelect").on("click", function() {
-	showPage("#home");
-});
+myApp.controller('brothersController', ['$scope', function($scope) {
+    
+    $scope.brothers = [
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        }
+    ];
+}]);
 
-$("#brothersSelect").on("click", function() {
-	showPage("#brothersPage");
-});
+myApp.controller('alumniController', ['$scope', function($scope) {
+        $scope.alumni = [
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        },
+        {
+            "name" : "John Doe",
+            "title" : "President",
+            "image" : "images/ato3.jpg"
+        }
+    ];
+}]);
 
-$("#alumniSelect").on("click", function() {
-	showPage("#alumniPage");
-});
+myApp.controller('aboutController', ['$scope', function($scope) {
+    
+}]);
 
-$("#aboutSelect").on("click", function() {
-	showPage("#aboutPage");
-});
-
-$("#contactSelect").on("click", function() {
-	showPage("#contactPage");
-});
+myApp.controller('contactController', ['$scope', function($scope) {
+    
+}]);
